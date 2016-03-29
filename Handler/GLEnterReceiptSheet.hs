6b785c7e2d77ef37d194757f99b91b6243b8a352
Maxime Bourget <bmx007@gmail.com>
2016-03-29 14:44:09 +0100

    Add GLEnterReceiptSheet via yesod add-handler command.

---
+++ Handler/GLEnterReceiptSheet.hs
@@ -0,0 +1,9 @@
+module Handler.GLEnterReceiptSheet where
+
+import Import
+
+getGLEnterReceiptSheetR :: Handler Html
+getGLEnterReceiptSheetR = error "Not yet implemented: getGLEnterReceiptSheetR"
+
+postGLEnterReceiptSheetR :: Handler Html
+postGLEnterReceiptSheetR = error "Not yet implemented: postGLEnterReceiptSheetR"
---
+++ test/Handler/GLEnterReceiptSheetSpec.hs
@@ -0,0 +1,14 @@
+module Handler.GLEnterReceiptSheetSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getGLEnterReceiptSheetR" $ do
+        error "Spec not implemented: getGLEnterReceiptSheetR"
+
+
+    describe "postGLEnterReceiptSheetR" $ do
+        error "Spec not implemented: postGLEnterReceiptSheetR"
+
