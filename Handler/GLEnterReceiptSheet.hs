85327447379adc3aa96473d1a144f3ded9466073
Maxime Bourget <bmx007@gmail.com>
2016-03-29 16:13:46 +0100

    Add basic enter receipts sheet page.

--- Handler/GLEnterReceiptSheet.hs
+++ Handler/GLEnterReceiptSheet.hs
@@ -1,9 +1,40 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Handler.GLEnterReceiptSheet where
 
 import Import
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3,
+                              withSmallInput)
+-- | Entry point to enter a receipts spreadsheet
+-- The use should be able to :
+--   - post a text
+--   - upload a document
+--   - use an existing document
+--   - download a spreadsheet template
+
+       
 
 getGLEnterReceiptSheetR :: Handler Html
-getGLEnterReceiptSheetR = error "Not yet implemented: getGLEnterReceiptSheetR"
+getGLEnterReceiptSheetR = do
+  (formWidget, formEncType) <- generateFormPost postTextForm
+  let widget =  [whamlet|
+<h1>Enter a receipts spreadsheet
+<ul>
+  <li> Enter text
+    <form role=form method=post action=@{GLEnterReceiptSheetR} enctype=#{formEncType}>
+      ^{formWidget}
+      <button type="submit" .btn .btn-default>Process
+  <li> Or Upload a document
+  Not implemented
+  <li> Or use an existing document
+  Not implemented
+  <li> Or download a spreadsheet template
+  Not implemented
+|]
+  defaultLayout $ widget
+
+postTextForm :: Form Text
+postTextForm = renderBootstrap3 BootstrapBasicForm $ fmap unTextarea (areq textareaField (withSmallInput "receipts") Nothing)
 
 postGLEnterReceiptSheetR :: Handler Html
 postGLEnterReceiptSheetR = error "Not yet implemented: postGLEnterReceiptSheetR"
