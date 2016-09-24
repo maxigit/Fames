3ed3d8bdd10018a4646de0a8d72d2176e5af359b
Maxime Bourget <bmx007@gmail.com>
2016-09-24 22:23:32 +0100

    Fix test broken following new generated routes.

--- test/Handler/FaSpec.hs
+++ test/Handler/FaSpec.hs
@@ -9,13 +9,13 @@ appSpec :: Spec
 appSpec = withAppNoDB BypassAuth $ do
   describe "FA bindings" $ do
     it "displays list of bank accounts" $ do
-      get FABankAccountsR
+      get (FA'R FABankAccountsR)
       statusIs 200
 
       bodyContains "Current account"
 
     it "displays list of users" $ do
-      get FAUsersR 
+      get (FA'R FAUsersR )
       statusIs 200
 
       bodyContains "admin"
