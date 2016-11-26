9fc12e6c10a00c7dd24625b3894142b27736ca0b
Maxime Bourget <bmx007@gmail.com>
2016-11-26 16:21:13 +0000

    Add missing FAMES file

---
+++ FAMES.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE FlexibleInstances, DeriveGeneric #-}
+-- Not needed at it duplicates all classes from model
+-- Just easier to get a quick view of db
+module FAMES where
+
+import ClassyPrelude.Yesod
+import Database.Persist.Quasi
+
+  
+share [mkPersist sqlSettings] -- , mkMigrate "migrateAll"]
+    $(persistFileWith lowerCaseSettings "config/fames-models")
