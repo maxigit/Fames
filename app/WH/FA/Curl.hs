bc351ab4355072ce77fb1601086f34da5f41da47
Maxime Bourget <bmx007@gmail.com>
2017-06-04 21:43:24 +0100

    Curl works but need to check errors from FA responses
    Also need to add ExceptT

--- app/WH/FA/Curl.hs
+++ app/WH/FA/Curl.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 -- | Post event to FA using Curl
 module WH.FA.Curl
 ( postStockAdjustment
@@ -8,47 +9,65 @@ import ClassyPrelude
 import WH.FA.Types
 import Network.Curl
 
+import qualified Prelude as Prelude
 
-postStockAdjustment = do
-  -- Init session
-  let opts = [CurlCookieJar "cookies" , CurlVerbose True ]
-      loginOptions = CurlPostFields [ "user_name_entry=curl"
-                                    , "password=purl"
+
+faURL = "http://127.0.0.1"
+inventoryAdjustmentURL = faURL <> "/inventory/adjustments.php"
+newAdjustmentURL = inventoryAdjustmentURL <> "?NewAdjustment=1"
+toAjax url = url <> "?jsHttpRequest=0-xml"
+ajaxInventoryAdjustmentURL = toAjax inventoryAdjustmentURL
+
+addAdjustmentDetail :: Curl -> StockAdjustmentDetail -> IO (Either Text ())
+addAdjustmentDetail curl StockAdjustmentDetail{..} = do
+  let items = CurlPostFields [ "AddItem=Add%20Item"
+                             , "stock_id="<> unpack sku
+                             , "std_cost=" <> show cost
+                             , "qty=" <> show quantity
+                             ] : method_POST
+  r <- do_curl_ curl ajaxInventoryAdjustmentURL items :: IO CurlResponse
+  return (Right ())
+  
+
+-- | Open a Session  to FrontAccounting an execute curl statement
+withFACurlDo :: String -> String -> String -> (Curl -> IO (Either Text a)) -> IO (Either Text a)
+withFACurlDo faURL user password m = do
+  let opts = [{-CurlCookieJar "cookies" ,-} CurlUserAgent "curl/7.47.0", CurlVerbose True ]
+  let loginOptions = CurlPostFields [ "user_name_entry_field="<>user
+                                    , "password=" <> password
                                     , "company_login_name=0"
                                     ] : method_POST
-      fa = "http:/127.0.0.1"
-      adjUrl = fa <> "/inventory/adjusment.php?jsHttpRequest=0-xml"
-
   withCurlDo $ do
     curl <- initialize
     setopts curl opts
-    r <- do_curl_ curl fa loginOptions :: IO CurlResponse
-    if respCurlCode r /= CurlOK || respStatus r /= 302
-       then error $ "Failed to log in: " ++ show (respCurlCode r) ++ respStatusLine r
+    r <- do_curl_ curl faURL (loginOptions <> [CurlCookieJar "cookies", CurlCookieSession True]) :: IO CurlResponse
+    setopts curl [CurlCookieFile "cookies"]
+    if respCurlCode r /= CurlOK || respStatus r /= 200
+       then return (Left "Failed to log in to FrontAccounting")
+       else m curl 
+
+postStockAdjustment ref stockAdj sku = do
+  withFACurlDo faURL "curl" "purl" $ \curl -> do
+    r <- do_curl_ curl newAdjustmentURL method_GET :: IO CurlResponse
+    if respCurlCode r /= CurlOK || respStatus r /= 200
+       then return (Left "Problem trying to create a new inventory adjustment")
        else do
-          let items = CurlPostFields [ "AddItem=Add%20Item"
-                                     , "stock_id=ML13-BD1-CHK"
-                                     , "std_cost=3.5"
-                                     , "qty=3"
-                                     ] : method_POST
-          r <- do_curl_ curl adjUrl items :: IO CurlResponse
-          if respCurlCode r /= CurlOK || respStatus r /= 200
-             then error $ "Failed to add object to session : " ++ show (respCurlCode r) ++ respStatusLine r
-             else do
-               let process = CurlPostFields [ "ref=test"
-                                            , "Process=Process"
-                                            , "AdjDate=2017/06/03"
-                                            , "StockLocation=DEF"
-                                            ] : method_POST
-               r <- do_curl_ curl adjUrl process :: IO CurlResponse
-               if respCurlCode r /= CurlOK || respStatus r /= 200
-                 then error $ "Failed to add process session : " ++ show (respCurlCode r) ++ respStatusLine r
-                 else do
-                     return ()
-      
-           
-      
-
-
-  
-  
+          r <- addAdjustmentDetail curl (StockAdjustmentDetail sku 1 5)
+          case r of
+            Left _ -> return r
+            Right _ ->  do
+              -- finalize
+                    let process = CurlPostFields [ "ref="++ref
+                                                  , "Process=Process"
+                                                  , "AdjDate=2017/06/03"
+                                                  , "StockLocation=DEF"
+                                                  ] : method_POST
+                    r <- do_curl_ curl ajaxInventoryAdjustmentURL process :: IO CurlResponse 
+                    if respCurlCode r /= CurlOK || respStatus r /= 200
+                      then return $ Left ("Failed to add process session : " <> tshow (respCurlCode r) ++ tshow (respStatusLine r))
+                      else do
+                        Prelude.putStrLn (respBody r)
+                        return (Right ())
+                
+          
+test ref = postStockAdjustment ref undefined
