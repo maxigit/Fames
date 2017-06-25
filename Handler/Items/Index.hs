308a77b1407a15b97647667242bb6cc0c8e86bbc
Maxime Bourget <bmx007@gmail.com>
2017-06-25 21:03:15 +0100

    Add link to item history in item index page

--- Handler/Items/Index.hs
+++ Handler/Items/Index.hs
@@ -47,106 +47,110 @@ filterE conv field (Just (RegexFilter regex)) =
   
 
 itemsTable :: Maybe FilterExpression -> Maybe FilterExpression -> Bool ->  Handler Widget
-itemsTable styleF varF showInactive = runDB $ do
-  let conv = StockMasterKey
-  styles <- case styleF of
-    Nothing -> do
-               setWarning "Please enter a styles filter expression (SQL like expression or regexp starting with '/'')"
-               return []
-    Just _ -> selectList (filterE conv FA.StockMasterId styleF
-                          ++ if showInactive then [] else [FA.StockMasterInactive ==. False]
-                         )
-                        [Asc FA.StockMasterId]
-  variations <- case varF of
-    Nothing -> return styles
-    Just _  -> selectList (filterE conv FA.StockMasterId varF <> [FA.StockMasterInactive ==. False ])
-                [Asc FA.StockMasterId]
+itemsTable styleF varF showInactive = do
+  renderUrl <- getUrlRenderParams
+  runDB $ do
+    let conv = StockMasterKey
+    styles <- case styleF of
+      Nothing -> do
+                setWarning "Please enter a styles filter expression (SQL like expression or regexp starting with '/'')"
+                return []
+      Just _ -> selectList (filterE conv FA.StockMasterId styleF
+                            ++ if showInactive then [] else [FA.StockMasterInactive ==. False]
+                          )
+                          [Asc FA.StockMasterId]
+    variations <- case varF of
+      Nothing -> return styles
+      Just _  -> selectList (filterE conv FA.StockMasterId varF <> [FA.StockMasterInactive ==. False ])
+                  [Asc FA.StockMasterId]
 
-  let columns = [ "stock_id"
-                , "categoryId"
-                , "taxTypeId"
-                , "description"
-                , "longDescription"
-                , "units"
-                , "mbFlag"
-                , "salesAccount"
-                , "cogsAccount"
-                , "inventoryAccount"
-                , "adjustmentAccount"
-                , "assemblyAccount"
-                , "dimensionId"
-                , "dimension2Id"
-                , "actualCost"
-                , "lastCost"
-                , "materialCost"
-                , "labourCost"
-                , "overheadCost"
-                , "inactive"
-                , "noSale"
-                , "editable"
-                ] :: [Text]
+    let columns = [ "stock_id"
+                  , "categoryId"
+                  , "taxTypeId"
+                  , "description"
+                  , "longDescription"
+                  , "units"
+                  , "mbFlag"
+                  , "salesAccount"
+                  , "cogsAccount"
+                  , "inventoryAccount"
+                  , "adjustmentAccount"
+                  , "assemblyAccount"
+                  , "dimensionId"
+                  , "dimension2Id"
+                  , "actualCost"
+                  , "lastCost"
+                  , "materialCost"
+                  , "labourCost"
+                  , "overheadCost"
+                  , "inactive"
+                  , "noSale"
+                  , "editable"
+                  ] :: [Text]
 
-  -- Church encoding ?
-  let itemToF :: ItemInfo StockMaster
-              -> (VariationStatus, ItemInfo (StockMasterInfo ((,) [Text])))
-              -> (Text -> Maybe (Html, [Text]) -- Html + classes per column
-                 , [Text]) -- classes for row
+    -- Church encoding ?
+    let itemToF :: ItemInfo StockMaster
+                -> (VariationStatus, ItemInfo (StockMasterInfo ((,) [Text])))
+                -> (Text -> Maybe (Html, [Text]) -- Html + classes per column
+                  , [Text]) -- classes for row
 
-      itemToF item0 (status , ItemInfo style var stock) =
-        let val col = case col of
-              "stock_id" -> Just ([], style <> "-" <> var)
-              "categoryId" -> Just ( tshow <$> smiCategoryId stock )
-              "taxTypeId" -> Just $ tshow <$> smiTaxTypeId stock 
-              "description" -> Just $ smiDescription stock 
-              "longDescription" -> Just $ smiLongDescription stock 
-              "units" -> Just $ smiUnits stock 
-              "mbFlag" -> Just $ smiMbFlag stock 
-              "salesAccount" -> Just $ smiSalesAccount stock 
-              "cogsAccount" -> Just $ smiCogsAccount stock 
-              "inventoryAccount" -> Just $ smiInventoryAccount stock 
-              "adjustmentAccount" -> Just $ smiAdjustmentAccount stock 
-              "assemblyAccount" -> Just $ smiAssemblyAccount stock 
-              "dimensionId" -> Just $ tshow <$> smiDimensionId stock 
-              "dimension2Id" -> Just $ tshow <$> smiDimension2Id stock 
-              "actualCost" -> Just $ tshow <$> smiActualCost stock 
-              "lastCost" -> Just $ tshow <$> smiLastCost stock 
-              "materialCost" -> Just $ tshow <$> smiMaterialCost stock 
-              "labourCost" -> Just $ tshow <$> smiLabourCost stock 
-              "overheadCost" -> Just $ tshow <$> smiOverheadCost stock 
-              "inactive" -> Just $ tshow <$> smiInactive stock 
-              "noSale" -> Just $ tshow <$> smiNoSale stock 
-              "editable" -> Just $ tshow <$> smiEditable stock 
-              _ -> Nothing
-            classes :: [Text]
-            classes = "style-" <> iiStyle item0:
-              case smiInactive stock of
-                           (_, True) -> ["text-muted"]
-                           _ -> []
-                      ++ case status of
-                           VarOk -> []
-                           VarMissing -> ["danger"]
-                           VarExtra -> ["info"]
-                    ++ if var == iiVariation item0
-                        then ["bg-info", "base"]
-                        else ["variation"]
-     
-        in (\col -> fmap (\(fieldClasses, v)
-                    -> (toHtml v, ("stock-master-"<>col):fieldClasses)
-                       ) (val col)
-           , classes
-           )
+        itemToF item0 (status , ItemInfo style var stock) =
+          let val col = case col of
+                "stock_id" -> let sku =  style <> "-" <> var
+                                  route = ItemsR $ ItemsHistoryR sku
+                              in Just ([], [hamlet|<a href=@{route}>#{sku}|] renderUrl )
+                "categoryId" -> Just (toHtml . tshow <$> smiCategoryId stock )
+                "taxTypeId" -> Just $ toHtml <$> smiTaxTypeId stock 
+                "description" -> Just $ toHtml <$>  smiDescription stock 
+                "longDescription" -> Just $ toHtml <$> smiLongDescription stock 
+                "units" -> Just $ toHtml <$>  smiUnits stock 
+                "mbFlag" -> Just $ toHtml <$>  smiMbFlag stock 
+                "salesAccount" -> Just $ toHtml <$>  smiSalesAccount stock 
+                "cogsAccount" -> Just $ toHtml <$>  smiCogsAccount stock 
+                "inventoryAccount" -> Just $ toHtml <$>  smiInventoryAccount stock 
+                "adjustmentAccount" -> Just $ toHtml <$>  smiAdjustmentAccount stock 
+                "assemblyAccount" -> Just $ toHtml <$>  smiAssemblyAccount stock 
+                "dimensionId" -> Just $ toHtml . tshow  <$> smiDimensionId stock 
+                "dimension2Id" -> Just $ toHtml . tshow <$> smiDimension2Id stock 
+                "actualCost" -> Just $ toHtml <$> smiActualCost stock 
+                "lastCost" -> Just $ toHtml <$> smiLastCost stock 
+                "materialCost" -> Just $ toHtml <$> smiMaterialCost stock 
+                "labourCost" -> Just $ toHtml <$> smiLabourCost stock 
+                "overheadCost" -> Just $ toHtml <$> smiOverheadCost stock 
+                "inactive" -> Just $ toHtml <$> smiInactive stock 
+                "noSale" -> Just $ toHtml <$> smiNoSale stock 
+                "editable" -> Just $ toHtml <$> smiEditable stock 
+                _ -> Nothing
+              classes :: [Text]
+              classes = "style-" <> iiStyle item0:
+                case smiInactive stock of
+                            (_, True) -> ["text-muted"]
+                            _ -> []
+                        ++ case status of
+                            VarOk -> []
+                            VarMissing -> ["danger"]
+                            VarExtra -> ["info"]
+                      ++ if var == iiVariation item0
+                          then ["bg-info", "base"]
+                          else ["variation"]
 
-      stockMasterToItem (Entity key val) = ItemInfo  style var val where
-                  sku = unStockMasterKey key
-                  style = take 8 sku
-                  var = drop 9 sku
-      itemStyles = map stockMasterToItem styles
-      itemVars =  map stockMasterToItem variations
-      itemGroups = joinStyleVariations itemStyles itemVars
-        
-  return $ displayTable columns
-                        (\c -> (toHtml c, []))
-                        (concatMap (\(base, aggregate, vars) -> map (itemToF base) vars) itemGroups)
+          in (\col -> fmap (\(fieldClasses, v)
+                      -> (v, ("stock-master-"<>col):fieldClasses)
+                        ) (val col)
+            , classes
+            )
+
+        stockMasterToItem (Entity key val) = ItemInfo  style var val where
+                    sku = unStockMasterKey key
+                    style = take 8 sku
+                    var = drop 9 sku
+        itemStyles = map stockMasterToItem styles
+        itemVars =  map stockMasterToItem variations
+        itemGroups = joinStyleVariations itemStyles itemVars
+
+    return $ displayTable columns
+                          (\c -> (toHtml c, []))
+                          (concatMap (\(base, aggregate, vars) -> map (itemToF base) vars) itemGroups)
                       
 
 -- * Rendering
