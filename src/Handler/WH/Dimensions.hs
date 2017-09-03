bb746e8880389662253aee167265401e420f774f
Maxime Bourget <bmx007@gmail.com>
2017-09-03 15:34:56 +0100

    Refactoring facet generation to deal with opening not on the top

--- src/Handler/WH/Dimensions.hs
+++ src/Handler/WH/Dimensions.hs
@@ -36,22 +36,24 @@ getWHDimensionR = do
 -- * Diagrams
 
 diag :: Diagram Cairo
-diag  = displayBox (Dimension 60 40 20 ) (Just $ Dimension  18 29 10) <> circle 5
+diag  = displayBox (Dimension 60 40 20 ) (Just $ Dimension  35 18 8) <> circle 5
 
 
 displayBox :: Dimension -> Maybe Dimension -> Diagram Cairo
 displayBox outer innerm   = let
-  facets = case innerm of
+  disp = map displayFacetISO
+  (fg_ , facets) = case innerm of
              -- no inner, or in fact no outer ...
-             Nothing -> innerBoxToFacets outer
+             Nothing -> ([], innerBoxToFacets up outer)
              Just inner -> let
                background = outerBoxToFacets outer
                foreground = outerBoxToFacets' outer
                inners = innerBoxes outer inner
                middle = mconcat $ map innerPToFacets inners
-               in foreground <> middle <> background
+               in (foreground , middle <> background)
   sortF = sortBy (comparing (facetZ)) 
-  in mconcat $ map displayFacetISO facets
+  in mconcat $ (disp fg_  # lc (blend 0.5 outerColourNY black) )
+               <> disp facets
 
 -- | Calculate 
 innerBoxes :: Dimension -> Dimension -> [PDimension]
@@ -88,18 +90,43 @@ outerBoxToFacets' (Dimension l w h) = let
   up = Facet (Dimension 0 0 h) Nothing  $ toDims [(0,0,0), (l,0,0), (l,w/2,0), (0,w/2,0)]
   in  reverse [front, frontside, up]
 
-innerBoxToFacets :: Dimension -> [Facet]
-innerBoxToFacets (Dimension l w h) = let
-  front = Facet (Dimension 0 w 0) (Just innerColourNY) $ toDims [(0,w,0), (l,w,0), (l,w,h), (0,w,h)]
-  frontside = Facet (Dimension l 0 0) (Just innerColourNZ)  $ toDims [(l,0,0), (l,w,0), (l,w,h), (l,0,h)]
-  up = Facet (Dimension 0 0 h) (Just innerColourNX)  $ toDims [(0,0,0), (l,0,0), (l,w/2,0), (0,w/2,0)]
-  up' = Facet (Dimension 0 (w/2) h) (Just innerColourNX)  $ toDims [(0,0,0), (l,0,0), (l,w/2,0), (0,w/2,0)]
-  in  [front, frontside, up, up']
+innerBoxToFacets :: Orientation -> Dimension -> [Facet]
+innerBoxToFacets opening (Dimension l w h) = let
+  top = facet (Dimension 0 0 h) (Just innerColourNX) [(0,0,0), (l,0,0), (l,w,0), (0,w,0)]
+  front = facet (Dimension 0 w 0) (Just innerColourNY) [(0,w,0), (l,w,0), (l,w,h), (0,w,h)]
+  side = facet (Dimension l 0 0) (Just innerColourNZ)  [(l,0,0), (l,w,0), (l,w,h), (l,0,h)]
+  facet off col points im jm km = [ Facet (off `mappend` offset)  col
+                                          [ Dimension  (l'/ls) (w'/ws) (h'/hs)
+                                          | (l',w',h') <- points
+                                          ]
+                                  | i <- [1..im]
+                                  , j <- [1..jm]
+                                  , k <- [1..km]
+                                  , let ls = fromIntegral im
+                                        ws = fromIntegral jm
+                                        hs = fromIntegral km
+                                        offset = ( Dimension (l/ls* fromIntegral (i-1))
+                                                   (w/ws * fromIntegral (j-1))
+                                                   (h/hs * fromIntegral (k -1))
+                                                 )
+                                  ]
+ 
+
+  go (Orientation Vertical Depth) = front 1 1 1 ++  side 1 1 1  ++ top 1 2 1
+  go (Orientation Vertical Horizontal) = front 1 1 1 ++  side 1 1 1 ++ top 2 1 1
+  go (Orientation Horizontal Depth) = front 1 1 1 ++  side 1 1 2 ++ top 1 1 1
+  go (Orientation Horizontal Vertical) = front 1 1 1 ++  side 1 2 1 ++ top 1 1 1
+  go (Orientation Depth Vertical) = front 1 1 2 ++  side 1 1 1 ++ top 1 1 1
+  in go opening
+
+-- halfL f@(Dimension l w h) = let
+--   f' = f { gg
+  
 
 innerPToFacets :: PDimension -> [Facet]
-innerPToFacets (PDimension off dim ori) = let
-  facets = innerBoxToFacets (W.rotate ori dim)
-  in map (translateFacet off) facets
+innerPToFacets (PDimension  off dim ori) = let
+  facets = innerBoxToFacets ori dim
+  in map (rotateFacet ori . translateFacet off) facets
   
 
  
@@ -131,7 +158,7 @@ outerColourNZ = sRGB24 224 244 245
 
 innerColourNY = sRGB24 192 148 59
 innerColourNX = sRGB24 224 201 153
-innerColourNZ = white --  sRGB24 235 219 189
+innerColourNZ = sRGB24 235 219 189
 
 cos30 = cos (pi/6)
 sin30 = sin (pi/6)
