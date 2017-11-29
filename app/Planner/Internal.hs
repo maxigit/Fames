5b72539e21e808c9a177b5c41f401d28e400b6ca
Maxime Bourget <bmx007@gmail.com>
2017-11-29 14:33:32 +0000

    Parsing warehouse scenario.

--- app/Planner/Internal.hs
+++ app/Planner/Internal.hs
@@ -1,13 +1,15 @@
 module Planner.Internal where
 
-import ClassyPrelude.Yesod
+import ClassyPrelude.Yesod hiding (Content)
 import WarehousePlanner.Display
 import WarehousePlanner.Base
+import Planner.Types
 import Control.Monad.ST (runST, stToIO, RealWorld)
 import Control.Monad.State (evalStateT,runStateT)
+import Data.Text(strip)
 
 import Unsafe.Coerce (unsafeCoerce)
-
+import Model.DocumentKey
 
 -- * Example
 warehouseExamble  = do
@@ -22,8 +24,64 @@ warehouseExamble  = do
   return $ ShelfGroup (map (ShelfProxy .shelfId) shelves) Vertical
 
 -- * Parsing
+-- | Read and cut a scenario file into different component
+parseScenarioFile :: Text -> Either Text [Section]
+parseScenarioFile text = do -- Either
+  lineTypes <- traverse parseLine (lines text) 
+  sequence $ linesToSections lineTypes
+  
+
+-- | Transform a line of text to a typed line
+parseLine :: Text -> Either Text TypedLine
+parseLine line | "-- END " `isPrefixOf` line          = Right EndL
+               | "-- " `isPrefixOf` line              = Right $ CommentL
+               | Just sha <- stripPrefix "@" line = Right $ HashL (DocumentHash sha)
+               | Just header <- stripPrefix "* " line  = case parseHeader header of
+                   Nothing -> Left $ line <> " is an invalid header type."
+                   Just h -> Right (HeaderL h)
+               | strip line                 == "" = Right $ CommentL
+               | otherwise                        = Right $ TextL line
+
+-- | Regroup lines into section
+linesToSections :: [TypedLine] -> [Either Text Section]
+linesToSections lines = reverse $ go lines MovesH [] [] where
+  go :: [TypedLine] -> HeaderType -> Content -> [Either Text Section] -> [Either Text Section]
+  go [] header current sections = merge header current sections
+  go ((CommentL):ls) header current sections = go ls header current sections
+  go ((EndL):_) header current sections = go [] header current sections
+  go ((HeaderL newHeader):ls) header current sections = -- start new section
+        go ls newHeader [] (merge header current sections)
+  go ((TextL txt):ls) header current sections = go ls header (Right txt:current) sections
+  -- Only one hash can be in a section. Close the previous section and open a new one at the same time
+  go ((HashL sha):ls) header [] sections = go ls header [] ((makeSection header [Left sha]) :sections)
+  go ls@((HashL _):_) header current sections = go ls header [] (go [] header current sections)
+  merge :: HeaderType  -> Content -> [Either Text Section] -> [Either Text Section]
+  merge header [] sections = sections
+  merge header current sections = (makeSection header (reverse current)) : sections
+
+makeSection :: HeaderType -> Content -> Either Text Section
+makeSection InitialH [Left sha] = Right (InitialS sha)
+makeSection InitialH  _         = Left "Initial section should be made of one and only one SHA"
+makeSection LayoutH content     = Right $ LayoutS content
+makeSection ShelvesH content    = Right $ ShelvesS content
+makeSection StocktakeH content  = Right $ ShelvesS content
+makeSection BoxesH content      = Right $ BoxesS content
+makeSection MovesH content      = Right $ MovesS content
+
+
+parseHeader :: Text -> Maybe HeaderType
+parseHeader h = case toLower (strip h) of
+  "layout" -> Just LayoutH
+  "shelves" -> Just ShelvesH
+  "initial" -> Just InitialH
+  "stocktake" -> Just StocktakeH
+  "boxes" -> Just BoxesH
+  "moves" -> Just MovesH
+  _ -> Nothing
+  
 -- warehouseFromOrg :: WH (ShelfGroup s)
 warehouseFromOrg text = warehouseExamble
+
 
 
 -- * Rendering
--- app/Planner/Types.hs
+++ app/Planner/Types.hs
@@ -1,1 +1,41 @@
 module Planner.Types where
+
+import ClassyPrelude
+import Model.DocumentKey
+import WarehousePlanner.Base
+
+
+type Content = [Either DocumentHash Text]
+-- * Content Types
+-- | AST-ish of a parsed document
+data Section = LayoutS Content -- describe the layout of the shelves. How to display them
+             | ShelvesS Content -- shelves description
+             | InitialS DocumentHash -- Initial state
+             | StocktakeS Content -- list of boxes and their location
+             | BoxesS Content -- list of boxes without location
+             | MovesS Content -- list of moves : boxes -> shelves
+       
+data TypedLine = CommentL 
+              | HeaderL HeaderType
+              | TextL Text
+              | EndL
+              | HashL DocumentHash
+              deriving (Show, Read)
+
+
+data HeaderType = LayoutH | ShelvesH | InitialH | StocktakeH | BoxesH | MovesH
+  deriving (Show, Read, Eq, Ord)
+
+-- * Scenario
+-- | Description of warehouse. Initial stage plus way modify it.
+-- It is only a description, .i.e elements are either text or a sha of it.
+data Scenario = Scenario
+  { sInitialState :: Maybe DocumentHash
+  , sSteps        :: [Step]
+  -- , sLayout ::  Either DocumentHash ByteString
+  } deriving (Read, Show)
+
+data Step = Step deriving (Read, Show)
+
+
+
