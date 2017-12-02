2097731e726ec3e52bd280bb07b81e12a88eadfc
Maxime Bourget <bmx007@gmail.com>
2017-12-02 15:31:00 +0000

    Displays box bars correctly when boxes overlaps.

--- legacy/WarehousePlanner/Display.hs
+++ legacy/WarehousePlanner/Display.hs
@@ -7,6 +7,7 @@ import Diagrams.Prelude hiding(Box)
 import Control.Monad.State
 import Diagrams.Backend.Cairo 
 import Data.Maybe
+import qualified Data.Map as Map
 
 display :: WH (Diagram B) s
 display = do
@@ -65,16 +66,16 @@ renderBoxes :: Shelf s -> WH (Diagram B) s
 renderBoxes shelf = let 
     in do
         boxes <- findBoxByShelf shelf
-        diags <-  mapM (renderBox shelf) boxes
-        return $ foldl atop mempty diags
-
+        z'diags <-  mapM (renderBox shelf) boxes
+        let zMap = Map.fromListWith atop (concat z'diags)
+        return $ foldl atop mempty (Map.elems zMap)
 
 depthBar :: Double -> Double -> Diagram B
 depthBar w used = let
-  shelfBar = rect (w/3) 5 # fc green # lwL 0
-  usedBar = rect (used/3) 5 # fc lightsteelblue # lwL 0
-  threshold1 = rect (w/3*0.70) 5 # fc red # lwL 0
-  threshold2 = rect (w/3*0.85) 5 # fc orange # lwL 0
+  shelfBar = depthBar' w 0 green -- rect (w/3) 5 # fc green # lwL 0
+  usedBar = depthBar' used 0 lightsteelblue -- rect (used/3) 5 # fc lightsteelblue # lwL 0
+  threshold1 = depthBar' (w*0.70) 0 red -- rect (w/3*0.70) 5 # fc red # lwL 0
+  threshold2 = depthBar' (w*0.85) 0 orange -- rect (w/3*0.85) 5 # fc orange # lwL 0
   in mconcat ( map alignL [ usedBar
                           , threshold1 
                           , threshold2 
@@ -82,11 +83,31 @@ depthBar w used = let
                           ]
              )
 
+depthBar' :: Double -> Double -> Colour Double -> Diagram B
+depthBar' width offset colour = depthBar'' (lwL 1 . lc colour) width offset colour
+depthBar'' :: (Diagram B -> Diagram B) -> Double -> Double -> Colour Double -> Diagram B
+depthBar'' up l l0 colour = translate (r2 (scale l0, 0)) . alignBL $ rect (scale l) 4 # fc colour # up
+  where scale x = x /3
+
 -- | render a Box within a shelf. 
 -- Boxes are stack vertically until they reach the maximum.
 -- to do so, we need to keep track of the postion of the last 
 -- box
-renderBox :: Shelf s -> Box s -> WH (Diagram B) s
+
+offsetBox :: Shelf s -> Box s -> Diagram B -> Diagram B
+offsetBox shelf box diagram  = let
+    Dimension xn yn zn = minDim shelf
+    Dimension xx yx zx = maxDim shelf
+    Dimension l w h = boxDim box
+    Dimension ox oy oz = boxOffset box
+
+    offset LeftToRight =  (ox+l/2, oz+h/2)
+    offset RightToLeft = (xn -ox-l/2, oz+h/2) 
+
+          
+    in diagram # translate (r2 (offset (flow shelf)))
+
+renderBox :: Shelf s -> Box s -> WH [(Int, Diagram B)] s
 renderBox shelf box = do
     let   Dimension xn yn zn = minDim shelf
           Dimension xx yx zx = maxDim shelf
@@ -95,24 +116,30 @@ renderBox shelf box = do
           foreground = if (ox+l) > xn || (oy+w) > yn || (oz+h) > zn
                   then firebrick
                   else black
-          bar = depthBar (yn) w  
-
-    
     background <-  gets colors `ap` return box
-
-
     let   r = rect l h  # lc foreground # fc background # lwL 2 #scale 0.95 # pad 1.05
           t = scaledText l h (showOrientation (orientation box) ++ boxSku box)
           diagram_ = t `atop` r
-          diagram = center $ (alignBL bar # translate (r2 (5, 5))) `atop` (alignBL diagram_)
-          offset LeftToRight =  (ox+l/2, oz+h/2)
-          offset RightToLeft = (xn -ox-l/2, oz+h/2) 
+          diagram = offsetBox shelf box diagram_
 
-          
-    return $ diagram # translate (r2 (offset (flow shelf)))
+          boxBar =  renderBoxBar box background
+          backBag = renderBoxBarBg shelf
+
+          offsetBar bar = bar # translate (r2 (5,5) ) # translate (r2 (ox, oz))
+    return $ [(3, diagram),
+              (2, offsetBar backBag),
+              (1, offsetBar boxBar)]
+
+renderBoxBar :: Box s -> Colour Double -> Diagram B
+renderBoxBar box background =
+  let Dimension l w h = boxDim box
+      Dimension ox oy oz = boxOffset box
+  in depthBar'' (lwL 1 . lc $ blend 0.5  black background ) w oy (blend 0.5 white background)
 
     
-    
+renderBoxBarBg :: Shelf s -> Diagram B
+renderBoxBarBg shelf = depthBar yn 0
+    where   Dimension xn yn zn = minDim shelf
     
 -- | draw text scaled to fit given rectangle
 -- scaledText :: Double -> Double-> String -> Diagram b
