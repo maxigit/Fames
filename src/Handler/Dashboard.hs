a7bf01ce0249d0c1c5339c8eef847739a767831f
Maxime Bourget <bmx007@gmail.com>
2018-06-20 19:11:21 +0100

    Implement rank pivot mode

--- src/Handler/Dashboard.hs
+++ src/Handler/Dashboard.hs
@@ -9,6 +9,8 @@ import Handler.Items.Reports.Common
 import Items.Types
 import GL.Utils
 import GL.Payroll.Settings
+import qualified Data.Map as Map
+import Formatting
 
 -- Display a dashboard made of different report depending on the configuration file
 getDMainR :: Handler Html
@@ -76,13 +78,13 @@ top100ItemYear which rupture = do
       rpFrom = Just beginYear
       rpTo = Just tomorrow
       rpPeriod' = Just PFSlidingYearFrom
-      rpNumberOfPeriods = Just 8
+      rpNumberOfPeriods = Just 2
       rpCategoryToFilter = Nothing
       rpCategoryFilter = Nothing
-      rpStockFilter = Just (LikeFilter "ML1_-A_2-BLK")
+      rpStockFilter = Nothing -- Just (LikeFilter "ML1_-A_2-BLK")
       rpPanelRupture = emptyRupture
       rpBand = emptyRupture
-      rpSerie = ColumnRupture (Just rupture) bestSalesTrace (Just RMResidual) (Just 5) False
+      rpSerie = ColumnRupture (Just rupture) bestSalesTrace (Just RMResidual) (Just 100) False
       rpColumnRupture = ColumnRupture  (Just periodColumn) (TraceParams QPSummary (Identifiable ("Column", [])) Nothing) Nothing Nothing True
       rpTraceParam2 = TraceParams QPSales (mkIdentifialParam amountOutOption) (Just $ NormalizeMode NMColumn NMSerie )
       rpTraceParam = TraceParams QPSales (mkIdentifialParam amountOutOption) (Just $ NormalizeMode NMRank NMBand )
@@ -99,7 +101,7 @@ top100ItemYear which rupture = do
   report <- if which 
             then itemReportXXX param grouper (\nmap -> panelPivotProcessorXXX nmap param "pivot-Top-100" nmap)
             -- else let pivotP tparams = processRupturesWith (\_ _ -> processRupturesWith (bandPivotProcessor tparams "pivot-Top-100") ) ()
-            else let pivotP tparams = processRupturesWith (createKeyRankProcessor $ \_ _ -> (bandPivotProcessor tparams "pivot-Top-100", \w -> [whamlet|<div#pivot-Top-100>^{w}|]) ) ()
+            else let pivotP tparams = processRupturesWith (createKeyRankProcessor $ \_ _ -> (bandPivotRankProcessor tparams "pivot-Top-100", \w -> [whamlet|<div#pivot-Top-100>^{w}|]) ) ()
                  in itemReport param pivotP--  (panelPivotProcessor "pivot-Top-100" (mkNMapKey "New Report"))
   return $ report
       
@@ -135,3 +137,108 @@ top100ItemYearChart plotName = do
                 ]
   report <- itemReportXXX param grouper (\nmap -> panelChartProcessor nmap param plotName nmap)
   return $ report
+
+-- pivotRankProcessor:: [TraceParams] -> _ColumnRuptures -> NMap TranQP -> Widget
+-- pivotRankProcessor tparams = 
+--   processRupturesWith (panelPivotRankProcessor tparams "items-report-pivotRank") ()
+  
+-- -- each nmap is a panel
+-- panelPivotRankProcessor :: [TraceParams] -> Text -> NMapKey -> Int -> _ -> _ -> NMap TranQP ->  Widget
+-- panelPivotRankProcessor tparams reportId = createKeyRankProcessor go where
+--   go key rank = let panelName = nkeyWithRank (rank, key)
+--                     panelId = reportId <> "-panel-" <> panelName
+--                     sub = bandPivotRankProcessor tparams panelId
+--                     widget children = [whamlet|
+--                                <div.panel.panel-info>
+--                                  <div.panel-heading data-toggle="collapse" data-target="#{panelId}">
+--                                    <h2>#{panelName}
+--                                  <div.panel-body.collapse.in id="#{panelId}" style="max-height:2000px; overflow:auto">
+--                                    ^{children}
+--                        |]
+--                     in (sub, widget)
+  
+
+-- each nmap is band
+bandPivotRankProcessor tparams panelId key rank parents ruptures = createKeyRankProcessor go key rank parents ruptures where
+  (serieRupture, (columRupture,_)) = ruptures
+  go key rank =  let sub = collectColumnsForPivotRank tparams
+                          -- get the list of the columns for that we need to
+                          -- sort and limit each serie first by calling processRupturesWith
+                     widget cw =  let
+                          (cols, rankMaps) = unzip cw
+                          -- colmns comes with a weight, we need to add up those weight sort
+                          -- the keys according to it
+                          columnMap = Map.fromListWith (<>) $ join cols
+                          sorted' = map fst (sortOn snd $ mapToList columnMap)
+                          sorted = if cpReverse columRupture then reverse sorted' else sorted'
+                          columns = zip [1..] $ sorted
+
+                          -- rankMap gives a map  rank , col widget to display, we need to assemble it first
+                          -- we should have any collision
+                          rankMap :: Map (Int, NMapKey) Widget 
+                          rankMap = mconcat  rankMaps
+                          ranks = maybe id take (cpLimitTo serieRupture) [1.. maximumEx (map fst $ keys rankMap)]
+                          in [whamlet|
+                              <div>
+                                <h3> #{nkeyWithRank (rank, key)}
+                                <table.table.table-border.table-hover.table-striped>
+                                  <thead>
+                                    <tr>
+                                      <th>
+                                      $forall column <- columns
+                                        <th.text90>#{nkeyWithRank column}
+                                    <tbody>
+                                      $forall rk <- ranks
+                                        <tr>
+                                          <td>#{rk}
+                                          $forall column <- columns 
+                                            <td>
+                                              ^{fromMaybe "-" (lookup (rk, snd column) rankMap)}
+                                        |]
+                     in (sub, widget)
+
+-- get the list of the columns for that we need to
+ -- nmap is a serie
+collectColumnsForPivotRank :: [TraceParams] -> NMapKey -> Int -> _parents -> _ruptures -> NMap TranQP -> [(_, Map (Int, NMapKey) Widget)]
+collectColumnsForPivotRank tparams key rank parents ruptures@(r, ()) nmap = let
+  (band, (panel, (all, ()))) = parents
+  -- we are within a serie, we need to get all the used columns
+  -- form nmap and return them upstream
+  columns = processRupturesWith (\k _ _mar _  n -> [(k, cpSorter r k (nmapMargin n) )]) parents ruptures nmap
+  -- columns = processRupturesWith (\k _ _mar _  n -> [(k, [key])]) parents ruptures nmap
+  formatPercent tp mode = case nmMargin <$>  mode of
+    Just NMRank -> \d -> let rank  = floor d
+                             isTop = rank == 1
+                             isTop2 = rank == 2
+                          in [shamlet|<span :isTop:.topOne :isTop2:.topTwo>#{sformat ords rank}|]
+
+    _ -> formatDouble' tp {tpValueType = VPercentage}
+  
+  -- all, we only need the first one, change format function to get an Integer (the rank)
+  rankTrace:_ = [formatSerieValuesNMap floor
+                                  floor
+                                  normMode all panel band
+                                  (fmap (tpValueGetter tp) . lookupGrouped qtype) (nmapRunSum (tpRunSum tp) nmap)
+           | TraceParams qtype tps normMode <- tparams
+           , tp <- getIdentified tps
+           ]
+  _:traces = [formatSerieValuesNMap (formatDouble' tp)
+                                  (formatPercent tp normMode)
+                                  normMode all panel band
+                                  (fmap (tpValueGetter tp) . lookupGrouped qtype) (nmapRunSum (tpRunSum tp) nmap)
+             | TraceParams qtype tps normMode <- tparams
+             , tp <- getIdentified tps
+             ]
+  rankMap = [ ((rankForCol, column) , widget rankForCol column) 
+            | (column, _) <- columns -- only use trace or nmap keys instead
+            , Just rankForCol <- return $ lookup column rankTrace
+            ]
+  widget rank column = let
+    isTop = rank == 1
+    isTop2 = rank == 2
+    in [whamlet|
+          <div :isTop:.topOne :isTop2:.topTwo>#{pvToText $ nkKey key}
+          $forall trace <- traces
+            <div.just-right>#{fromMaybe "-" $ lookup column trace}
+                        |]
+  in [(columns, mapFromList rankMap )]
