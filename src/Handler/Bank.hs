3dbec474f8a90dac66efbb0e9e884d86690c3a76
Maxime Bourget <bmx007@gmail.com>
2018-06-27 10:44:43 +0100

    Add link to fa transaction in statement view

--- src/Handler/Bank.hs
+++ src/Handler/Bank.hs
@@ -24,16 +24,17 @@ postGLBankR :: Handler Html
 postGLBankR = do
   today <- utctDay <$> liftIO getCurrentTime
   dbConf <- appDatabaseConf <$> getsYesod appSettings
+  faURL <- getsYesod (pack . appFAExternalURL . appSettings)
   -- get mysql connect info
   let options = B.Options{..}
-      hsbcFiles ="HSBC*.csv"
+      hsbcFiles ="HSBC*.csvX"
       faCredential = myConnInfo dbConf
-      statementFiles = "statement*.csv"
+      statementFiles = "statement*.csvX"
       output = ""
       startDate = Just (fromGregorian 2015 11 16 )
-      endDate = Just today
+      endDate = Nothing -- Just today
       faMode = B.BankAccountId 1
-      aggregateMode = B.TAIL
+      aggregateMode = B.BEST
 
       statmentPath = "/home/max/mae/warehouse/BankReconciliate"
 
@@ -41,11 +42,12 @@ postGLBankR = do
   
   stransz <- lift $ withCurrentDirectory statmentPath (B.main options)
   -- we sort by date
-  let sorted = sortOn ((Down . B._sAmount)) stransz
+  let sorted = sortOn ((Down . B._sDate)) stransz
   defaultLayout [whamlet|
     <table.table.table-hover.table-border.table-striped>
       <tr>
-        <th>Amount
+        <th>Paid Out
+        <th>Paid In
         <th>Source
         <th>Date
         <th>Type
@@ -54,15 +56,37 @@ postGLBankR = do
         <th>Object
        $forall trans <- sorted
          $with isFa <- B._sSource trans == B.FA
-          <tr :isFa:.text-danger.bg-danger>
-              <td>#{tshow $  B._sAmount trans}
+          <tr :isFa:.text-danger :isFa:.bg-danger>
+              $if B._sAmount trans > 0
+                <td>
+                <td>#{tshow $  B._sAmount trans}
+              $else
+                <td>#{tshow $ negate  $    B._sAmount trans}
+                <td>
               <td>#{tshow $ B._sSource trans}
               <td>#{tshow $ B._sDate trans}
-              <td>#{B._sType trans}
+              <td>^{linkToFA (urlForFA faURL) trans}
               <td>#{B._sDescription trans}
               <td>#{maybe "-" tshow $ B._sNumber trans}
-              <td>#{fromMaybe "" $ B._sObject trans}
+              <td>#{fromMaybe "-" (B._sObject trans)}
                         |]
+
+linkToFA
+  :: (FATransType -> Int -> Text) -> B.STrans -> Html
+linkToFA urlForFA' trans = case (readMay (B._sType trans), B._sNumber trans) of
+  (Just t, Just no) | B._sSource trans == B.FA ->
+           let eType = toEnum t
+               trans = showTransType eType :: Text
+           in [shamlet|
+     <a href="#{urlForFA' (eType) no}" target=_blank>
+       #{trans}
+       |]
+  _ -> toHtml (B._sType trans)
+  
 
 
       
+showType :: String ->  String
+showType ttype =  case readMay ttype of
+  Nothing -> ttype
+  Just n -> showTransType $ toEnum n
