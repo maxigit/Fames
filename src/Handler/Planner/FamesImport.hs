b57f841f10e1515456e53832a2046ebd6bf63886
Maxime Bourget <bmx007@gmail.com>
2019-09-11 17:45:07 +0100

    Planner: FamesImport dispatch using Yesod Route
    Not really different from pattern matching manually
    but it saves us of checking the type are correct

--- src/Handler/Planner/FamesImport.hs
+++ src/Handler/Planner/FamesImport.hs
@@ -8,22 +8,36 @@ import qualified Handler.WH.PackingList as PL
 import qualified Handler.WH.Boxtake as Box
 import Database.Persist.Sql (toSqlKey)
 import Data.Conduit.List (consume)
+import Data.Text(splitOn)
 -- * Type
-data FamesImport
-  = ImportPackingList (Key PackingList) -- ^ import packing list
-  -- | BoxTag
-  deriving (Eq, Show)
+-- data FamesImport
+--   = ImportPackingList (Key PackingList) -- ^ import packing list
+--   -- | BoxTag
+--   deriving (Eq, Show)
 
+-- | Create a Route type for each type of import
+-- This is a just a quick way of generate `parseRoute`
+-- which helps us to convert a URI to data type
+-- that we can dispatch on later
+data FI
+mkYesodSubData "FI" [parseRoutes|
+/packinglist/#Int64 FIPackingList GET
+/activeBoxes FIActiveBoxes GET
+|]
 -- *  Dispatcher
 importFamesDispatch :: Section -> Handler (Either Text [Section])
 importFamesDispatch (Section ImportH (Right content) _) = do
-  sections <- forM content $ \pl' ->  do
-    case readMay pl' of
-      -- Nothing -> return $ Left $ "importFamesDispatch: " <> tshow pl'  <> " is not  a valid packing list id"
-      Nothing -> Right <$> importActiveBoxtakes []
-      Just plId -> Right <$> importPackingList (toSqlKey plId) []
+  sections <- forM content $ \uri ->  do
+    let (main:tags) = splitOn "#" uri
+        pieces = splitOn "/" main
+    case (parseRoute (pieces, [])) of
+      Nothing -> return $ Left $ uri <> " is not a valid import"
+      Just fi -> Right <$> case fi of
+          FIPackingList plId -> importPackingList (toSqlKey plId) tags
+          FIActiveBoxes -> importActiveBoxtakes tags
   return $ sequence sections
 importFamesDispatch section = return $ Right [section]
+
 
 -- * Importers
 -- | Imports undelivered boxes from packing list.
@@ -43,17 +57,22 @@ importActiveBoxtakes :: [Text] -> Handler Section
 importActiveBoxtakes tags = do
   today <- todayH
   let source = Box.boxSourceToCsv Box.plannerSource
-  content <- take 20 <$>  (runDB $ runConduit $ source .| consume)
+  content <- (runDB $ runConduit $ source .| consume)
   return $ Section (StocktakeH tags) (Right content) ("* Stocktake from Planner [" <> tshow today <> "]")
 
   
 
 
 -- | Generate a TAG files from barcode
--- with scan and status information
+-- with scan and status information.
+-- The main difference with ActiveBoxtakes
+-- is that it doesn't create boxes but only tags
 importBoxStatus = return []
   
   
   
 
   
+
+
+
