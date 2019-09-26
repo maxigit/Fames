88852f6034c71345f05104c17fcd46486ef5f6e5
Maxime Bourget <bmx007@gmail.com>
2019-09-26 14:16:29 +0100

    Planner: Add more palettes

--- app/Planner/Colour.hs
+++ app/Planner/Colour.hs
@@ -5,6 +5,7 @@ module Planner.Colour
 where
 
 import Data.Colour.Palette.BrewerSet
+import Data.Colour.Palette.ColorSet
 import ClassyPrelude
 import Prelude((!!))
 
@@ -13,7 +14,7 @@ readFromPalette :: Text -> Maybe Kolor
 readFromPalette name =
   case break (== '-')  name of
     (paletteName, (uncons -> Just ('-', indexs))) | Just i <- readMay indexs -> let
-                              palettem = readBrewerSet paletteName
+                              palettem = readBrewerSet paletteName <|> readColorSet paletteName
                               in palettem <*> (Just $ i-1)
     _ -> Nothing
 -- * Brewer Set
@@ -33,6 +34,18 @@ readBrewerSet name =
     [(n,  cat)] -> Just (\i -> brewerSet cat n !! (i `mod` n))
     _ -> Nothing
     
+-- * Read from Palette.ColorSet
+readColorSet :: Text -> Maybe (Int -> Kolor)
+readColorSet "rybColor" = Just (\i -> rybColor (i `mod` 24))
+readColorSet "wheel24" = Just (\i -> rybColor (i `mod` 24))
+readColorSet "d3Colors1" = Just (\i -> d3Colors1 (i `mod` 10))
+readColorSet "d3Colors21" = Just (\i -> d3Colors2 Light (i `mod` 10))
+readColorSet "d3Colors22" = Just (\i -> d3Colors2 Dark (i `mod` 10))
+readColorSet "d3Colors41" = Just (\i -> d3Colors4 Lightest (i `mod` 10))
+readColorSet "d3Colors42" = Just (\i -> d3Colors4 Light (i `mod` 10))
+readColorSet "d3Colors43" = Just (\i -> d3Colors4 Dark (i `mod` 10))
+readColorSet "d3Colors44" = Just (\i -> d3Colors4 Darkest (i `mod` 10))
+readColorSet _ = Nothing
 
 
 
