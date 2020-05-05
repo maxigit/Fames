03c67252e40edc05c77efa63c35e82f3fc0d51a4
Maxime Bourget <bmx007@gmail.com>
2020-05-05 17:00:20 +0100

    Remove traces

--- legacy/BankReconciliate.hs
+++ legacy/BankReconciliate.hs
@@ -174,7 +174,6 @@ fetchFA balance0 cinfo bankAccount startM endM = do
              balance = (fromMaybe 0 balance0) - prevBal
              (_, results) = mapAccumL tupleToFATransaction (balance, 1) rows
     -- writeFile "/home/max/Webshot/fetched.hs" (fromString $ show ("RESULT", results, "Rows" , rows, "balance0", balance0, "befores", befores, "prev", prevBal, "balance", balance))
-    writeFile "/home/max/Webshot/fetched.hs" (fromString $ show ("RESULT [", results, "] Rows[" , rows, "] balance0 [", balance0, "] befores [", befores, " ]prev [", prevBal, "balance", balance, startM, endM))
     return results
     where q0 = "SELECT type, trans_no, ref, trans_date, CAST(person_id as CHAR(100)), amount, reconciled"
                          ++ " FROM 0_bank_trans"
