c3ec6b4aca5e3e1aac0456ea6a6c3332cca6a560
Maxime Bourget <bmx007@gmail.com>
2020-07-05 12:01:18 +0100

    AIS: group transaction and keep them in order

--- src/Handler/GL/CheckAIS.hs
+++ src/Handler/GL/CheckAIS.hs
@@ -11,10 +11,7 @@ import FA
 -- import GL.Utils
 
 data CheckParam = CheckParam
-  { invoiceIds :: Maybe Text
-  , invoiceRefs :: Maybe Text
-  , creditIds :: Maybe Text
-  , creditRefs :: Maybe Text
+  { groups :: Maybe Text
   }
 
 getGLCheckAISR :: Handler Html
@@ -31,16 +28,15 @@ postGLCheckAISR = do
       renderCheck (Just param) (Just w)
 
 checkForm paramM = renderBootstrap3 BootstrapBasicForm form where
-  form = CheckParam <$> aopt textField "invoice ids" (invoiceIds <$> paramM)
-                    <*> aopt textField "invoice references " (invoiceRefs <$> paramM)
-                    <*> aopt textField "credits ids " (creditIds <$> paramM)
-                    <*> aopt textField "credits references  " (creditRefs <$> paramM)
+  form = CheckParam <$> aopt textField "transactions" (groups <$> paramM)
 
 -- | Common page between GET and POST
 renderCheck :: Maybe CheckParam -> Maybe Widget -> Handler Html 
 renderCheck param widgetM = do
   ((formW), encType) <- generateFormPost (checkForm param)
   defaultLayout $ [whamlet|
+$newline text
+  ^{info}
 <form #render-check-form role=form method=post action=@{GLR GLCheckAISR} enctype=#{encType}>
   <div.well>
     ^{ formW }
@@ -50,24 +46,32 @@ renderCheck param widgetM = do
                           |]
 
 
+info = infoPanel "Info"
+  [whamlet|
+  Please enter the transaction as a <verbatim>,</verbatim{ separated list
+  of invoice or credit note (prefixed with <verbatim>-</verbatim>.
+  A transaction will be looked by reference unless it start with a <verbatim>#</verbatim>.
+  In that case it will be looked up by id.
+
+  Example
+
+      <code> 1 -#2  , 3
+
+   Will load 2 groups. The first made of
+   invoice with <b>reference</b> 1 and credit note with id 2
+   and the second group invoice with reference 3
+  |]
 
  -- | The real function where the checks are done and rendered to a Widget
 processCheck :: CheckParam -> Handler Widget
 processCheck CheckParam{..} = do
   faURL <- getsYesod (pack . appFAExternalURL . appSettings)
   customerName_ <- entityNameMH True
-  let iids = mapMaybe readMay (maybe [] (splitOn ",") invoiceIds)
-      irefs = maybe [] (splitOn ",") invoiceRefs
-      cids = mapMaybe readMay (maybe [] (splitOn ",") creditIds)
-      crefs = maybe [] (splitOn ",") creditRefs
-      urlFn = urlForFA faURL
+  let urlFn = urlForFA faURL
       customerName tran = let debtor = debtorTranDebtorNo tran
                           in fromMaybe (tshow debtor) $ customerName_ (toEnum $ debtorTranType tran) (fromIntegral <$> debtor)
-  invoices <- loadCustomerTransaction ST_SALESINVOICE iids
-  credits <- loadCustomerTransaction ST_CUSTCREDIT cids
-  invoicesByRef <- loadCustomerTransactionByRef ST_SALESINVOICE irefs
-  creditsByRef <- loadCustomerTransactionByRef ST_CUSTCREDIT crefs
-  let trans = invoices ++ credits ++ invoicesByRef ++ creditsByRef
+  transGroups <- loadTransactionGroups groups
+  let trans  = concat transGroups
       summary = [whamlet|
   <table *{datatable}>
     <thead>
@@ -82,7 +86,7 @@ processCheck CheckParam{..} = do
         <th data-class-name="text-right"> Tax Goods
         <th data-class-name="text-right"> Shipping Tax
 
-    $forall (Entity _ tran) <- trans
+    $forall tran <- trans
       <tr>
         <td> #{transNoWithLink urlFn "" (toEnum $ debtorTranType tran) (debtorTranTransNo tran) }
         <td> #{transactionIcon (toEnum $ debtorTranType tran)}
@@ -95,15 +99,16 @@ processCheck CheckParam{..} = do
         <td> #{formatDouble (debtorTranOvFreightTax tran)}
  |]
 
-  return $ primaryPanel "Summary" summary >> primaryPanel "AIS" (aisRemittanceAdvice customerName trans)
+  return $ primaryPanel "Summary" summary >> primaryPanel "AIS" (aisRemittanceAdvice customerName transGroups)
 
 -- | Display information to match AIS remittance advice
 -- so we can easily spot the difference
-aisRemittanceAdvice :: (DebtorTran -> Text) -> [Entity DebtorTran] -> Widget
-aisRemittanceAdvice customerName trans =  
-  let  tosum = map (aisBilling . entityVal) trans ++ map (totalInvoice . entityVal)  trans
+aisRemittanceAdvice :: (DebtorTran -> Text) -> [[DebtorTran]] -> Widget
+aisRemittanceAdvice customerName groups =  
+  let  tosum = map (aisBilling) groups ++ map (totalInvoice)  trans
        isPos = (>0)
        isNeg = (<0)
+       trans = concat groups
   in [whamlet|
   <table *{datatable}>
     <thead>
@@ -120,7 +125,7 @@ aisRemittanceAdvice customerName trans =
         <th data-class-name="text-right"> Total Debit
         <th data-class-name="text-right"> Total Credit
 
-    $forall (Entity _ tran) <- trans
+    $forall group <- groups
       $# ais billing
       <tr>
         <td.c0> 
@@ -128,34 +133,35 @@ aisRemittanceAdvice customerName trans =
         <td.c2> AIS BILING
         <td.c3>
         <td.c4> -
-        <td.c5> #{formatDouble (aisBilling tran)}
+        <td.c5> #{formatDouble (aisBilling group)}
         <td.c6> 
-        <td.c7> #{formatDouble (aisBillingNet tran)}
-        <td.c8> #{formatDouble (aisBillingTax tran)}
-        #{showDebitCredit (aisBilling tran)}
+        <td.c7> #{formatDouble (aisBillingNet group)}
+        <td.c8> #{formatDouble (aisBillingTax group)}
+        #{showDebitCredit (aisBilling group)}
         $# customer invoice
-      <tr>
-        <td.c0> #{tshow (debtorTranTranDate tran)}
-        <td.c1> #{debtorTranReference tran}
-        <td.c2> #{customerName tran}
-        <td.c3>
-        <td.c4> #{formatDouble (aisDiscount tran)}
-        <td.c5> #{formatDouble (totalInvoice tran)}
-        <td.c6> #{formatDouble (debtorTranOvAmount tran)}
-        <td.c7> #{formatDouble (debtorTranOvFreight tran)}
-        <td.c8> #{formatDouble (liftA2 (+) debtorTranOvGst debtorTranOvFreightTax  tran)}
-        #{showDebitCredit (totalInvoice tran)}
+      $forall tran <- group
+        <tr>
+          <td.c0> #{tshow (debtorTranTranDate tran)}
+          <td.c1> #{debtorTranReference tran}
+          <td.c2> #{customerName tran}
+          <td.c3>
+          <td.c4> #{formatDouble (aisDiscount' tran)}
+          <td.c5> #{formatDouble (totalInvoice tran)}
+          <td.c6> #{formatDouble (debtorTranOvAmount tran)}
+          <td.c7> #{formatDouble (debtorTranOvFreight tran)}
+          <td.c8> #{formatDouble (liftA2 (+) debtorTranOvGst debtorTranOvFreightTax  tran)}
+          #{showDebitCredit (totalInvoice tran)}
     $# total
     <tr>
       <td.c0>
       <td.c1>
       <td.c2>
       <td.c3>
-      <td.c4> #{formatDouble (sum $ map (aisDiscount . entityVal) trans)}
-      <td.c5> #{formatDouble (sum $ map (liftA2 (+) aisBilling totalInvoice . entityVal)  trans)}
-      <th.c6> #{formatDouble (sum $ map (debtorTranOvAmount . entityVal)  trans)}
-      <th.c7> #{formatDouble (sum $ map (liftA2 (+) aisBillingNet debtorTranOvFreight . entityVal)  trans)}
-      <th.c8> #{formatDouble (sum $ map (liftA3 (add3) aisBillingTax debtorTranOvGst debtorTranOvFreightTax . entityVal)  trans)}
+      <td.c4> #{formatDouble (aisDiscount trans)}
+      <td.c5> #{formatDouble (sum $ map (liftA2 (+) aisBilling' totalInvoice)  trans)}
+      <th.c6> #{formatDouble (sum $ map (debtorTranOvAmount)  trans)}
+      <th.c7> #{formatDouble (sum $ map (liftA2 (+) aisBillingNet' debtorTranOvFreight)  trans)}
+      <th.c8> #{formatDouble (sum $ map (liftA3 (add3) aisBillingTax' debtorTranOvGst debtorTranOvFreightTax)  trans)}
       <th> #{formatDouble (0 - sum (filter isNeg tosum))}
       <th> #{formatDouble (sum (filter isPos tosum))}
     <tr>
@@ -165,21 +171,28 @@ aisRemittanceAdvice customerName trans =
     <tr>
       <th colspan=9>
       <th> total discount
-      <th> #{formatDouble (sum $ map (aisDiscount . entityVal) trans) }
+      <th> #{formatDouble (aisDiscount trans) }
     <tr>
       <th colspan=9>
       <th> to be paid
-      <th.bg-success> #{formatDouble $ sum tosum - (sum $ map (aisDiscount . entityVal) trans) }
+      <th.bg-success> #{formatDouble $ sum tosum - (aisDiscount trans) }
  |]
 
 add3 a b c = a + b + c
-aisDiscount :: DebtorTran -> Double
-aisDiscount trans@DebtorTran{..} = (debtorTranOvAmount ) * 0.06  * (1 + taxRate trans)
+aisDiscount' :: DebtorTran -> Double
+aisDiscount' trans@DebtorTran{..} = (debtorTranOvAmount ) * 0.06  * (1 + taxRate trans)
 taxRate DebtorTran{..} = debtorTranOvGst / debtorTranOvAmount 
 totalInvoice DebtorTran{..} = debtorTranOvAmount + debtorTranOvFreight + debtorTranOvFreightTax + debtorTranOvGst - debtorTranOvDiscount
-aisBillingNet DebtorTran{..} = - debtorTranOvAmount * 0.01
-aisBillingTax DebtorTran{..} = - debtorTranOvAmount * 0.01 * 0.2
-aisBilling DebtorTran{..} = - debtorTranOvAmount * 0.01 * 1.20 -- VAT even if the customer is Ireland : it is AIS which is invoicing
+aisBillingNet' DebtorTran{..} = - debtorTranOvAmount * 0.01
+aisBillingTax' DebtorTran{..} = - debtorTranOvAmount * 0.01 * 0.2
+aisBilling' DebtorTran{..} = - debtorTranOvAmount * 0.01 * 1.20 -- VAT even if the customer is Ireland : it is AIS which is invoicing
+
+
+aisDiscount = sumMap aisDiscount'
+aisBillingNet = sumMap aisBillingNet'
+aisBillingTax = sumMap aisBillingTax'
+aisBilling = sumMap aisBilling'
+sumMap f = sum . map f
 
 
 
@@ -197,21 +210,30 @@ showDebitCredit amount =
       |]
     
 
-loadCustomerTransaction :: FATransType  -> [Int] -> Handler [(Entity DebtorTran)]
-loadCustomerTransaction type_ ids = do
-  runDB $ map (reverseIfCredit type_) <$> selectList [DebtorTranTransNo <-. ids, DebtorTranType ==. fromEnum type_ ] [Asc DebtorTranTranDate]
-  
-loadCustomerTransactionByRef :: FATransType  -> [Text] -> Handler [(Entity DebtorTran)]
-loadCustomerTransactionByRef type_ refs = do
-  runDB $ map (reverseIfCredit type_) <$> selectList [DebtorTranReference <-. refs, DebtorTranType ==. fromEnum type_ ] [Asc DebtorTranTranDate]
+loadTransactionGroups :: Maybe Text -> Handler [[DebtorTran]]
+loadTransactionGroups Nothing = return []
+loadTransactionGroups (Just groups) = mapM loadTransactionGroup (splitOn (",") groups)
 
-reverseIfCredit ST_CUSTCREDIT e@(Entity _ DebtorTran{..}) =
- e {entityVal = DebtorTran{debtorTranOvAmount= -debtorTranOvAmount
-                      ,debtorTranOvFreight= -debtorTranOvFreight
-                      ,debtorTranOvFreightTax= -debtorTranOvFreightTax
-                      ,debtorTranOvGst= -debtorTranOvGst
-                      ,..}
-                      }
+loadTransactionGroup :: Text -> Handler [DebtorTran]
+loadTransactionGroup group = catMaybes <$> mapM loadTransaction (splitOn " " group)
+
+loadTransaction :: Text -> Handler (Maybe DebtorTran)
+loadTransaction tran = do
+  let (type_, tran')  = case stripPrefix "-" tran of
+                          Nothing -> (ST_SALESINVOICE, tran)
+                          Just t -> (ST_CUSTCREDIT, t)
+      selector = case stripPrefix "#" tran' >>= readMay of
+                    Nothing -> DebtorTranReference ==. tran'
+                    Just tid -> DebtorTranTransNo ==. tid
+  fmap (reverseIfCredit type_ .  entityVal) . headMay
+    <$> runDB (selectList [DebtorTranType ==. fromEnum type_, selector] [])
+  
+reverseIfCredit ST_CUSTCREDIT DebtorTran{..} =
+   DebtorTran{debtorTranOvAmount= -debtorTranOvAmount
+             ,debtorTranOvFreight= -debtorTranOvFreight
+             ,debtorTranOvFreightTax= -debtorTranOvFreightTax
+             ,debtorTranOvGst= -debtorTranOvGst
+             ,..}
 reverseIfCredit _ t = t 
 
 
