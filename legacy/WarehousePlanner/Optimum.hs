578de2a33145f003d8c5798a9251201ada5410da
Maxime Bourget <bmx007@gmail.com>
2023-01-07 13:21:29 +0000

    Planner: Remove dead code

--- legacy/WarehousePlanner/Optimum.hs
+++ legacy/WarehousePlanner/Optimum.hs
@@ -6,8 +6,6 @@ import Prelude
 import WarehousePlanner.Base
 import Data.List(sortBy)
 import Data.Function(on)
-import qualified Data.Map.Strict as Map'
-import Control.Monad.State
 
 data Ranking = Rank3D 
              | Rank2D -- ^ ignores the depth when sorting and reporting % of use. Handy if we know we can adjust the depth of a shelf.
@@ -29,32 +27,3 @@ usedRatio Rank2D box s n _ m = let
     bdim = boxDim box
     sdim = minDim s
     in (fromIntegral (n*m))*(dLength bdim * dHeight bdim) / (dLength sdim * dHeight sdim)
-
-_fillBest :: PartitionMode -> SortBoxes -> (Box s -> [OrientationStrategy] -> [Shelf s] -> [Shelf s])
-          -> [Box s] 
-          -> [Shelf s]
-          -> WH [Box s] s
-_fillBest  pmode smode fit boxes shelves = do
-    aroundArrangement Nothing smode (fillBest' pmode smode fit) boxes shelves
-
-fillBest' :: Shelf' shelf =>
-             PartitionMode ->
-             SortBoxes -> 
-             (Box s -> [OrientationStrategy] -> t -> [shelf s])
-          -> [Box s] -> t -> WH [Box s] s
-fillBest' pmode smode fit boxes shelves = do
-        boxo <- gets boxOrientations
-        let groups = reverse . Map'.toList $ Map'.fromListWith (++) tuples
-            tuples = [((_boxDim b, boxStyle b), [b]) | b <- boxes]
-        boxess <- mapM ( \bs -> let
-                shelvesInOrder = fit example ors shelves
-                ors = boxo example (error "need one shelf and only one")
-                example = head bs
-                -- in moveBoxes [example] shelvesInOrder
-                in moveBoxes ExitLeft pmode smode bs shelvesInOrder
-                ) (sortBy (compare `on` (\g -> - (length g))) (map snd groups))
-
-        return $ concat boxess
-
-    
-    
