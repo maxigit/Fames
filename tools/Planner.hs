ecbec53cae2386c9f41403a08fec123864279a35
Maxime Bourget <bmx007@gmail.com>
2024-04-10 15:55:29 +0100

    Planner: Fix static dir not found

--- tools/Planner.hs
+++ tools/Planner.hs
@@ -1,13 +1,14 @@
 import ClassyPrelude hiding (getArgs)
 import qualified WarehousePlanner.Main as W
 import Handler.Planner.FamesImport
-import Application (handler)
+import Application (handlerWith)
 import System.Environment(getArgs, withArgs)
+import Settings(AppSettings(..))
 
 
 main:: IO ()
 main = do
   args <- getArgs
   let (yml, others) = partition (".yml" `isSuffixOf`) args
-  let dispatch path section = withArgs yml $ handler (importFamesDispatch' path section)
+  let dispatch path section = withArgs yml $ handlerWith (\s -> s { appStaticDir = "."}) (importFamesDispatch' path section)
   withArgs others $ W.defaultMainWith dispatch
