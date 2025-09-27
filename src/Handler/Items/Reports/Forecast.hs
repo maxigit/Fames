5603178cdf9fc456e9a30d29e3c40f65d18fdd3a
Maxime Bourget <bmx007@gmail.com>
2025-09-27 18:21:05 +0100

    Speed up query

--- src/Handler/Items/Reports/Forecast.hs
+++ src/Handler/Items/Reports/Forecast.hs
@@ -133,12 +133,22 @@ loadActualCumulSalesByWeek :: Day -> SqlConduit () (Sku, [(Amount, Int64)]) () -
 loadActualCumulSalesByWeek end = do
    -- todo adjust to beginning of week
    let start = calculateDate (AddYears $ -1) end
-   let sql = "SELECT stock_id, sum(quantity*unit_price), YEARWEEK(tran_date,5) as week ":
+   let _sql = "SELECT stock_id, sum(qty_done*unit_price), YEARWEEK(tran_date,5) as week ":
            " FROM 0_debtor_trans_details " :
-           " JOIN 0_debtor_trans ON (0_debtor_trans_details.debtor_trans_no = 0_debtor_trans.trans_no) " :
+           " JOIN 0_debtor_trans ON (0_debtor_trans_details.debtor_trans_no = 0_debtor_trans.trans_no AND debtor_trans_type = type) " :
            " JOIN 0_stock_moves using(trans_no, type, stock_id, tran_date)" : -- ignore credit note without move => damaged
            " WHERE type IN ("  : (tshow $ fromEnum ST_CUSTDELIVERY) : ",": (tshow $ fromEnum ST_CUSTCREDIT) : ") " :
-           " AND quantity != 0" :
+           " AND qty_done != 0" :
+           -- " AND stock_id = ?" :
+           " AND tran_date >= ? AND tran_date < ? " :
+           " GROUP BY stock_id, week " :
+           " order BY stock_id, week " :
+           []
+   -- only use stock moves to make it much faster
+   let sql = "SELECT stock_id, sum(qty*price), YEARWEEK(tran_date,5) as week ":
+           " FROM 0_stock_moves " :
+           " WHERE type IN ("  : (tshow $ fromEnum ST_CUSTDELIVERY) : ",": (tshow $ fromEnum ST_CUSTCREDIT) : ") " :
+           " AND qty != 0" :
            -- " AND stock_id = ?" :
            " AND tran_date >= ? AND tran_date < ? " :
            " GROUP BY stock_id, week " :
