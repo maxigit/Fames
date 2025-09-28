6497c620a5e2dfcd860b685155882d96454cb889
Maxime Bourget <bmx007@gmail.com>
2025-09-28 12:07:57 +0100

    amend! Works with help of C...
    Works with help of C...

--- src/Handler/Items/Reports/NewForecast.hs
+++ src/Handler/Items/Reports/NewForecast.hs
@@ -29,7 +29,7 @@ plotForecastError actuals naive naiveUps naiveDowns  = do -- actuals naiveForeca
       , [{ 
          x: #{toJSON x}
          , y:#{toJSON naiveDownsY}
-         , line: {shape: "spline", color: "black"}
+         , line: {shape: "spline", color: "transparent"}
          }
          ,
          { 
@@ -43,14 +43,14 @@ plotForecastError actuals naive naiveUps naiveDowns  = do -- actuals naiveForeca
          x: #{toJSON x}
          , y:#{toJSON naive}
          , mode: "lines"
-         , line: {dash: "dot", color: "black"}
+         , line: {dash: "dash", color: "black"}
          }
          ,
          { 
          x: #{toJSON x}
          , y:#{toJSON actuals}
          , mode: "lines"
-         , line: {color: "C1"}
+         , line: {color: "green"}
          }
          ]
       );
@@ -70,7 +70,6 @@ getPlotForecastError end = do
              runConduit $ 
              alignConduit salesSource naiveSource
                  .|mapC  joinWithZeros
-                 .|mapC traceShowId
                  .|mapC  addErrors
                  .| C.foldl1 \(salesA, naiveA, (upA, downA)) (salesB, naiveB, (upB, downB)) -> 
                           (salesA `vadd` salesB
@@ -80,7 +79,6 @@ getPlotForecastError end = do
 
         
 
-        traceShowM (naive, salesByWeek)
         let plot = plotForecastError salesByWeek  naive ups downs
         return [whamlet|
           ^{plot}
